9429bd6cb11b4cbd12a3e6d02aaf61f93cae52f1
Aline Augusto <lineaugusto87@gmail.com>
2024-05-23 12:27:39 -0300

    mudança no git ignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '3zp6ku',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    video: true,
+    
   },
 });
